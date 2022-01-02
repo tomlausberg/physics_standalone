209b585183df65824e12b83d0d60eee513e99254
Tom Lausberg <toml@ethz.ch>
2022-01-02 13:25:15 +1100

    cleanup put data script

--- radiation/put_data.sh
+++ radiation/put_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#directory containing google cloud bucket data
 export DATA_DIR=$1
  
 
@@ -9,96 +10,67 @@ else
 
     export MYHOME=`pwd`
 
-    if [ ! -d "./fortran/data" ]; then
-        cd ./fortran
-        mkdir data
-        cd data
-        mkdir LW
-        mkdir SW
-        cd $MYHOME
-    else
-        echo "Fortran output directory already exists"
-    fi
+    mkdir -p fortran
+    cd ./fortran
+    mkdir -p data
+    cd data
+    mkdir -p LW
+    mkdir -p SW
+    cd ..
+    mkdir -p radlw
+    mkdir -p radsw
 
-    if [ -z "$(ls -A ./fortran/data/LW)" ]; then
-        cp -r $DATA_DIR/fv3gfs-fortran-output/lwrad/* ./fortran/data/LW/.
-        cd ./fortran/data/LW
-        tar -xzvf data.tar.gz
-        cd $MYHOME
-    else
-        echo "LW Fortran data already present"
-    fi
-
-    if [ -z "$(ls -A ./fortran/data/SW)" ]; then
-        cp -r $DATA_DIR/fv3gfs-fortran-output/swrad/* ./fortran/data/SW/.
-        cd ./fortran/data/SW
-        tar -xzvf data.tar.gz
-        cd $MYHOME
-    else
-        echo "SW Fortran data already present"
-    fi
-
-    cd ./python
-    mkdir lookupdata
     cd $MYHOME
 
-    if [ -z "$(ls -A ./python/lookupdata)" ]; then
-        cp -r $DATA_DIR/lookupdata/lookup.tar.gz ./python/lookupdata/.
-        cd ./python/lookupdata
-        tar -xzvf lookup.tar.gz
-        cd $MYHOME
-    else
-        echo "Data already present"
-    fi
+    cp -r $DATA_DIR/fv3gfs-fortran-output/lwrad/* ./fortran/data/LW/.
+    cd ./fortran/data/LW
+    tar -xzvf data.tar.gz
+    cd $MYHOME   
 
-    if [ ! -d "./fortran/radlw/dump" ]; then
-        cd ./fortran/radlw
-        mkdir dump
-        cd $MYHOME
-    else
-        echo "LW standalone output directory already exists"
-    fi
+    cp -r $DATA_DIR/fv3gfs-fortran-output/swrad/* ./fortran/data/SW/.
+    cd ./fortran/data/SW
+    tar -xzvf data.tar.gz
+    cd $MYHOME
 
-    if [ ! -d "./fortran/radsw/dump" ]; then
-        cd ./fortran/radsw
-        mkdir dump
-        cd $MYHOME
-    else
-        echo "SW standalone output directory already exists"
-    fi
+    mkdir -p python
+    cd ./python
+    mkdir -p lookupdata
+    cd $MYHOME
 
-    if [ -z "$(ls -A ./fortran/radlw/dump)" ]; then
-        cp $DATA_DIR/standalone-output/lwrad/* ./fortran/radlw/dump/.
-        cd ./fortran/radlw/dump
-        tar -xzvf data.tar.gz
-        cd $MYHOME
-    else
-        echo "LW standalone data already present"
-    fi
+    cp -r $DATA_DIR/lookupdata/lookup.tar.gz ./python/lookupdata/.
+    cd ./python/lookupdata
+    tar -xzvf lookup.tar.gz
+    cd $MYHOME
 
-    if [ -z "$(ls -A ./fortran/radsw/dump)" ]; then
-        cp $DATA_DIR/standalone-output/swrad/* ./fortran/radsw/dump/.
-        cd ./fortran/radsw/dump
-        tar -xzvf data.tar.gz
-        cd $MYHOME
-    else
-        echo "SW standalone data already present"
-    fi
+    cd ./fortran/radlw
+    mkdir -p dump
+    cd $MYHOME
 
-    if [ ! -d "./python/forcing" ]; then
-        cd ./python
-        mkdir forcing
-        cd $MYHOME
-    else
-        echo "Forcing directory already exists"
-    fi
+    cd ./fortran/radsw
+    mkdir -p dump
+    cd $MYHOME
 
-    if [ -z "$(ls -A ./python/forcing)" ]; then
-	    cp $DATA_DIR/forcing/* ./python/forcing/.
-	    cd ./python/forcing
-	    tar -xzvf data.tar.gz
-        cd $MYHOME
-    else
-	    echo "Forcing data already present"
-    fi  
+    cp -r $DATA_DIR/standalone-output/lwrad/* ./fortran/radlw/dump/.
+    cd ./fortran/radlw/dump
+    tar -xzvf data.tar.gz
+    cd $MYHOME
+
+    cp $DATA_DIR/standalone-output/swrad/* ./fortran/radsw/dump/.
+    cd ./fortran/radsw/dump
+    tar -xzvf data.tar.gz
+    cd $MYHOME
+
+    cd ./python
+    mkdir -p forcing
+    cd $MYHOME
+
+    cp $DATA_DIR/forcing/* ./python/forcing/.
+    cd ./python/forcing
+    tar -xzvf data.tar.gz
+    cd $MYHOME
+    
+    cd ./fortran
+    mkdir -p radlw
+    cd ./radlw
+    cp -r $DATA_DIR/fortran_data .
 fi
