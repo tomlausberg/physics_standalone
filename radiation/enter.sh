c96111d4443f0cfa59d6f34f494d3b17986ef605
Tom Lausberg <tlausber@daint104.login.cscs.ch>
2022-01-05 01:24:07 +0100

    update daint enviroment

--- radiation/enter.sh
+++ radiation/enter.sh
@@ -2,5 +2,6 @@ source ~/venv/bin/activate
 source ~/env.daint.sh
 export PYTHONPATH=/users/tlausber/gt4py/install:$PYTHONPATH
 export BACKEND="gtc:gt:gpu"
-export RADIATION_INPUT="~/toml_standalone/radiation/"
+export RADIATION_INPUT="/users/tlausber/toml_standalone/radiation/"
 export IS_DAINT=True
+export IS_DOCKER=False
